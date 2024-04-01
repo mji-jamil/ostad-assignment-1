1dc5be6de2a519ec8115cf1da8b85c0fc8cf25a7
Jamil <jamiulislamjamil@gmail.com>
2024-04-01 11:38:50 +0600

    changes

--- bank_app/BackAccount.js
+++ bank_app/BackAccount.js
@@ -13,7 +13,7 @@ class BankAccount {
 
     withdraw(amount) {
         if (amount > this.balance) {
-            console.log("Insufficient funds");
+            console.log("Insufficient funds.");
         } else {
             this.balance -= amount;
             console.log(`Withdrawn $${amount}. New balance: $${this.balance}`);
@@ -33,21 +33,16 @@ class BankAccount {
 
 const account1 = new BankAccount("Jamil", 1000);
 const account2 = new BankAccount("Jamil2", 500);
-// const account3 = new BankAccount("Jamil3", 2000);
 
+console.log(`Balance: ${account1.getBalance()}`);
 account1.deposit(200);
 account1.withdraw(300);
 account1.displayAccountInfo();
-// console.log(account1.getBalance());
+
 
 console.log("............");
 
+console.log(`Balance: ${account2.getBalance()}`);
 account2.deposit(100);
 account2.withdraw(700);
 account2.displayAccountInfo();
-// console.log(account2.getBalance());
-
-// console.log(".............");
-//
-// account3.displayAccountInfo();
-// account3.deposit(1500);
