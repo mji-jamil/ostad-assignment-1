baa17aac4a6ac8b1a3f599ce3d970ec7843e1737
Jamil <jamiulislamjamil@gmail.com>
2024-04-01 10:51:05 +0600

    changes

--- bank_app/BackAccount.js
+++ bank_app/BackAccount.js
@@ -33,21 +33,21 @@ class BankAccount {
 
 const account1 = new BankAccount("Jamil", 1000);
 const account2 = new BankAccount("Jamil2", 500);
-const account3 = new BankAccount("Jamil3", 2000);
+// const account3 = new BankAccount("Jamil3", 2000);
 
 account1.deposit(200);
 account1.withdraw(300);
 account1.displayAccountInfo();
-console.log(account1.getBalance());
+// console.log(account1.getBalance());
 
 console.log("............");
 
 account2.deposit(100);
 account2.withdraw(700);
 account2.displayAccountInfo();
-console.log(account2.getBalance());
+// console.log(account2.getBalance());
 
-console.log(".............");
-
-account3.displayAccountInfo();
-account3.deposit(1500);
+// console.log(".............");
+//
+// account3.displayAccountInfo();
+// account3.deposit(1500);
