0db94747e210800f10b15fb9cc80a9b4cf289e41
Jamil <jamiulislamjamil@gmail.com>
2024-04-07 16:21:11 +0600

    changes

--- calculator/script.js
+++ calculator/script.js
@@ -2,12 +2,16 @@ const num1Input = document.getElementById("number1");
 const num2Input = document.getElementById("number2");
 const selectAction = document.getElementById("actions");
 const calculateBtn = document.querySelector(".btn-primary");
-const resultValue = document.querySelector(".result-value");
 
 function calculate() {
     const num1 = parseFloat(num1Input.value);
     const num2 = parseFloat(num2Input.value);
     const action = selectAction.value;
+
+    if (isNaN(num1) || isNaN(num2)) {
+        document.getElementById('result').innerHTML = '<div class="alert alert-danger p-2 mt-4 fs-4" role="alert">Please enter both numbers!</div>';
+        return;
+    }
 
     let result;
     switch (action) {
@@ -21,13 +25,18 @@ function calculate() {
             result = num1 * num2;
             break;
         case "/":
+            if (num2 === 0) {
+                document.getElementById('result').innerHTML = '<div class="alert alert-danger p-2 mt-4 fs-4" role="alert">Cannot divide by zero!</div>';
+                return;
+            }
             result = num1 / num2;
             break;
         default:
-            result = "Invalid action";
+            document.getElementById('result').innerHTML = '<div class="alert alert-danger p-2 mt-4 fs-4" role="alert">Invalid operation!</div>';
+            return;
     }
 
-    resultValue.textContent = `Result: ${result}`;
+    document.getElementById('result').innerHTML = `<div class="p-2 fs-4 alert alert-success mt-4" role="alert">Result: ${result}</div>`;
 }
 
 calculateBtn.addEventListener("click", calculate);
